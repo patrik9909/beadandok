fd7af6e998b309515809cce72f6188ecbd94fc44
patrik9909 <49908251+patrik9909@users.noreply.github.com>
2019-05-10 01:16:48 +0200

    Update 34.feladat.py

--- 34.feladat.py
+++ 34.feladat.py
@@ -1,16 +1,20 @@
+try:
 
-infile=open("be.txt","r")
-outfile=open("output.py","w")
-lista=[]
-for i in infile:
-    lista.append(i)
-    print (lista[0],file=outfile)
-infile.close()
-outfile.close()
-fajl="output.py"
+      infile=open("be.txt","r")
+      outfile=open("output.py","w")
+      lista=[]
+      for i in infile:
+            lista.append(i)
+            print (lista[0],file=outfile)
+      infile.close()
+      outfile.close()
+      fajl="output.py"
 
-filename=open(fajl,"r").read()
-compile(filename,fajl,"exec")
+      filename=open(fajl,"r").read()
+      compile(filename,fajl,"exec")
+      print("jo kod")
+except:
+      print("hibás kód")
 
 
 
