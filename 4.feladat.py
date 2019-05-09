f86dbb87cee5a36b8f22f4e3fe63849350d32754
patrik9909 <49908251+patrik9909@users.noreply.github.com>
2019-05-09 22:09:46 +0200

    Add files via upload

---
+++ 4.feladat.py
@@ -0,0 +1,13 @@
+def timer(datum):
+      from datetime import datetime
+      datumkon=datum.split("-")
+      l=[]
+      date_format = "%Y/%m/%d/"
+      ido = datetime(int(datumkon[0]), int(datumkon[1]), int(datumkon[2]))
+      miku = datetime(2019, 12, 6)
+      delta = miku - ido
+      final= delta.days
+      l.append(final)
+      print(l[0],"napot kell még aludni mikulásig")
+print(timer("2015-02-07"))
+
