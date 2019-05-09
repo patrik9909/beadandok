780294a2aaf18500933e597c5078b600e108250a
patrik9909 <49908251+patrik9909@users.noreply.github.com>
2019-05-09 22:05:51 +0200

    Add files via upload

---
+++ 34.feladat.py
@@ -0,0 +1,16 @@
+
+infile=open("be.txt","r")
+outfile=open("output.py","w")
+lista=[]
+for i in infile:
+    lista.append(i)
+    print (lista[0],file=outfile)
+infile.close()
+outfile.close()
+fajl="output.py"
+
+filename=open(fajl,"r").read()
+compile(filename,fajl,"exec")
+
+
+
